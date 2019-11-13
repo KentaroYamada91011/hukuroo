1ed0193a50132a49726ad3467872e16255e304a8
Kentaro91011 <kentaro.10.11@icloud.com>
2019-11-14 01:49:16 +0900

    fix: style

--- app/helpers/spree/base_helper.rb
+++ app/helpers/spree/base_helper.rb
@@ -102,7 +102,7 @@ module Spree
         taxons = root_taxon.children.map do |taxon|
           css_class = (current_taxon && current_taxon.self_and_ancestors.include?(taxon)) ? 'current' : nil
           content_tag :li, class: css_class do
-           link_to(taxon.name, seo_url(taxon), class:"fronted__taxon__link") +
+           link_to(taxon.name, seo_url(taxon), class:"fronted__taxon__link__child") +
              taxons_tree(taxon, current_taxon, max_level - 1)
           end
         end
