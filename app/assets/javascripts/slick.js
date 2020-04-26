b9464e56445c79a5e22b5109eaa3c8184141211a
山田健太郎 <kentaro.10.11@icloud.com>
2020-04-26 17:49:17 +0900

    add: style

--- app/assets/javascripts/slick.js
+++ app/assets/javascripts/slick.js
@@ -1,16 +1,32 @@
 $(document).ready(function(){
-  console.log($('#slider'));
-  $('#slider').slick({
-    autoplay:false,
-    autoplaySpeed:2000,
-    dots:false,
-    arrows: true,
-    nextArrow: '<button type="button" class="slick-next"></button>',
-    prevArrow: '<button type="button" class="slick-prev"></button>',
-		infinite: true,
-		slidesToShow: 3,
-    slidesToScroll: 1,
-    centerMode: true,
-    centerPadding: '25%'
-  });
+  console.log(screen.width>400);
+  if (screen.width>750) {
+    $('#slider').slick({
+      autoplay:false,
+      autoplaySpeed:2000,
+      dots:false,
+      arrows: true,
+      nextArrow: '<button type="button" class="slick-next"></button>',
+      prevArrow: '<button type="button" class="slick-prev"></button>',
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      centerMode: true,
+      centerPadding: '25%'
+    });
+  } else {
+    $('#slider').slick({
+      autoplay:false,
+      autoplaySpeed:2000,
+      dots:false,
+      arrows: true,
+      nextArrow: '<button type="button" class="slick-next"></button>',
+      prevArrow: '<button type="button" class="slick-prev"></button>',
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      centerMode: true,
+      centerPadding: '5%'
+    });
+  }
 });
--- app/controllers/spree/home_controller_decorator.rb
+++ app/controllers/spree/home_controller_decorator.rb
@@ -2,6 +2,7 @@ Spree::HomeController.class_eval do
   def index
     @searcher = build_searcher(params.merge(include_images: true))
     @products = @searcher.retrieve_products.limit(5)
+    @products_sp = @searcher.retrieve_products.limit(6)
     @taxonomies = Spree::Taxonomy.includes(root: :children)
     @top_page = true
   end
