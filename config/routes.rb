1d3e818eb99a87ba93796220e09c3c303d301b3c
山田健太郎 <kentaro.10.11@icloud.com>
2020-05-03 18:51:12 +0900

    add: user page

--- config/routes.rb
+++ config/routes.rb
@@ -21,5 +21,7 @@ Rails.application.routes.draw do
     resources :purchased_items,  only: %i(new create), controller: 'purchased_items'
   end
 
+  get '/account/address'        => 'spree/users#address'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
